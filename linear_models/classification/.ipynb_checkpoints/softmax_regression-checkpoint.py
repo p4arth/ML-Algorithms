46da33a79f91785ab58f22e49841caecc7c6ba36
Paarth Bhatnagar <paarthbhatnagarh3h3@gmail.com>
2022-07-07 23:33:12 +0530

    improved code readability.

--- linear_models/classification/.ipynb_checkpoints/softmax_regression-checkpoint.py
+++ linear_models/classification/.ipynb_checkpoints/softmax_regression-checkpoint.py
@@ -21,6 +21,25 @@ class SoftMaxRegression():
                  alpha = 0,
                  threshold = 0.5, 
                  optimizer = 'GD'):
+        '''
+        This function initializes the softmax regression
+        object.
+        Parameters:
+            penalty: 
+                The type of regularization that can 
+                be added to the model. Valid Values
+                "l2" and "None". Default: None.
+            alpha:
+                The regularization rate. Default: 0.
+            threshold:
+                The threshold value above which a data
+                point is classified belonging to a
+                particular class. Default: 0.5.
+            optimizer:
+                The type of optimizer to be used. Valid
+                values are "GD", "MBGD", and "SGD".
+                Default: "GD".
+        '''
         self.penalty = penalty
         self.optimizer = optimizer
         self.threshold = threshold
@@ -30,20 +49,36 @@ class SoftMaxRegression():
             self.alpha = 0
     
     def softmax(self, z):
+        '''
+        This applies the softmax normalization to
+        a vector.
+        Parameters:
+            z:
+                The matrix that needs to be soft-maxxed.
+        '''
         softmax_vector = []
+        # Softmax normalization applied on every vector in z.
         for v in z:
             lc = np.exp(v)/np.sum(np.exp(v))
             softmax_vector.append(lc)
         return np.array(softmax_vector)
     
     def cross_entropy_loss(self, X, W, Y):
+        '''
+        This function calculates the cross-entropy loss.
+        '''
         assert X.shape[-1] == W.shape[0], 'Incompatible shapes'
+        # Softmax after multipying X with W.
         A = self.softmax(X @ W)
         A_log = np.log(A)
         log_proba = np.sum(Y * A_log, axis=1)
         return (1/Y.shape[0]) * np.sum(log_proba)
     
     def cross_entropy_gradient(self, X, W, Y):
+        '''
+        This function calculates the gradient
+        of the softmax function.
+        '''
         return X.T @ (self.softmax(X @ W) - Y)
     
     def fit(self,
@@ -54,6 +89,29 @@ class SoftMaxRegression():
             lr = 0.01, 
             batch_size = 0,
             fit_intercept = False):
+        '''
+        This function fits the softmax regression
+        object.
+        Parameters:
+            X: 
+                The feature matrix.
+            y:
+                The one hot encoded corresponding 
+                class labels.
+            verbose:
+                The level of verbosity.
+            epochs:
+                The number of epochs for which the
+                optimizer should run.
+            lr:
+                The learning rate for the optimizer.
+            batch_size:
+                The batch size to take while using
+                the Mini Batch Gradient Descent.
+            fit_intercept:
+                If True it adds a dummy feature to
+                the feature matrix X.
+        '''
         self.intercept = fit_intercept
         if self.intercept:
             X = add_dummy_feature(X)
@@ -94,6 +152,10 @@ class SoftMaxRegression():
             self.weights = sgd.all_weights
     
     def predict_probas(self, X):
+        '''
+        This function returns the probability of each
+        class label belonging to the corresponding class.
+        '''
         if self.intercept:
             X = add_dummy_feature(X)
         A_p = self.softmax(X@self.optimized_weights)
@@ -101,6 +163,11 @@ class SoftMaxRegression():
         return probas
     
     def predict(self, X):
+        '''
+        This function predicts the class to which a
+        specific data point belongs to using the 
+        optimized weights obtained from fit method.
+        '''
         if self.intercept:
             X = add_dummy_feature(X)
         A_p = self.softmax(X@self.optimized_weights)
