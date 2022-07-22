400fad04c5478986a2bc13232f48638790bc078a
Paarth Bhatnagar <paarthbhatnagarh3h3@gmail.com>
2022-07-22 23:02:33 +0530

    added helpers for neural nets

---
+++ neural_networks/utils/helpers.py
@@ -0,0 +1,39 @@
+import numpy as np
+###### =======LOSSES======= #####
+def cross_entropy_loss(ytrue, yhat):
+    return -np.sum(ytrue * np.log(yhat))
+
+def sse_loss(ytrue, yhat):
+    return 0.5 * np.sum(np.square(y_hat - y))
+
+
+###### =======ACTIVATIONS======= #####
+def softmax(z):
+    softmaxed = []
+    for v in z:
+        ex = np.exp(v) / np.sum(np.exp(v))
+        softmaxed.append(ex)
+    return np.array(softmaxed)
+
+def identity(z):
+    return z
+
+def relu(z):
+    return np.where(z >= 0 , z, 0)
+
+def sigmoid(z):
+    return 1 / (1 + np.exo(-z))
+
+
+###### =======GRADIENTS======= #####
+def grad_sigmoid(z):
+    return sigmoid(z) * (1- sigmoid(z))
+
+def grad_relu(z):
+    return np.where(z >= 0, 1, 0)
+
+def grad_softmax(ytrue, yhat):
+    return ytrue - yhat
+
+def grad_sse(X, w, y):
+    return X.T @ (X@w - y)
